63c543c0ef58868ee78a8f5eeeca82d38152d0d2
Pragun Bhutani <pragun06@gmail.com>
2014-11-25 01:54:31 +0530

    and a star is born

---
+++ sirius/js/scripts.js
@@ -0,0 +1,16 @@
+var images = [
+	'appetizers.jpg',
+	'beer.jpg',
+	'burger.jpg',
+	'champagne.jpg',
+	'pizza.jpg',
+	'sunset.jpg',
+	'bottles.jpg',
+	'cheese.jpg',
+	'cocktail.jpg',
+	'smartphone.jpg'	
+];
+
+var imagePath = 'img/bg/' + images[ Math.floor( Math.random() * images.length ) ];
+
+document.getElementById( "bg-container" ).style.backgroundImage = "url(" + imagePath + ')';
