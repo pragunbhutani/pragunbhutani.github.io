ea08d280a8a233d49153d2c4ddbb7574390ad4af
Pragun Bhutani <pragun06@gmail.com>
2015-02-01 01:53:38 +0530

    added motor control

--- rigel/app.js
+++ rigel/app.js
@@ -3,7 +3,7 @@ var app = angular.module( 'rose', [ 'firebase' ] );
 app.controller( 'mainCtrl', [ '$scope', '$firebase', 
 	function( $scope, $firebase ) {
 
-		var ref = new Firebase( 'https://rigel.firebaseIO.com/experiments/sensor1' );
+		var ref = new Firebase( 'https://rigel.firebaseIO.com/experiments' );
 		var sync = $firebase( ref );
 
 		//download data into a local object
@@ -11,6 +11,18 @@ app.controller( 'mainCtrl', [ '$scope', '$firebase',
 
 		//tie to a scope variable
 		syncObject.$bindTo( $scope, 'firebaseData' );
+
+		$scope.toggleSensor = function() {
+			$scope.firebaseData.sensor.state = !$scope.firebaseData.sensor.state;
+		};
+
+		$scope.toggleLed = function() {
+			$scope.firebaseData.led.state = !$scope.firebaseData.led.state;
+		};
+
+		$scope.controlMotor = function( value ) {
+			$scope.firebaseData.motor.action = value;
+		};
 
 	}
 ]);
