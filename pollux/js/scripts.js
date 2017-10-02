5f7a9f3520e83a95e79e409889491c31b35eaa10
Pragun Bhutani <pragun06@gmail.com>
2017-10-02 18:26:44 +0200

    random marketing message + lightness

--- pollux/js/scripts.js
+++ pollux/js/scripts.js
@@ -1,17 +1,30 @@
-var images = [
-  'appetizers.jpg',
-  'beer.jpg',
-  'blue-house.jpg',
-  'green-tracks.jpg',
-  'hedge-cars.jpg',
-  'mall-fountain.jpg',
-  'night-store.jpg',
-  'pizza.jpg',
-  'sports-field.jpg',
-  'tuk-tuk.jpg',
-  'veg-vendor.jpg',
-];
+$(document).ready(function() {
 
-var imagePath = 'img/bg/' + images[ Math.floor( Math.random() * images.length ) ];
+  var images = [
+    'appetizers.jpg',
+    'beer.jpg',
+    'blue-house.jpg',
+    'green-tracks.jpg',
+    'hedge-cars.jpg',
+    'mall-fountain.jpg',
+    'night-store.jpg',
+    'pizza.jpg',
+    'sports-field.jpg',
+    'tuk-tuk.jpg',
+    'veg-vendor.jpg',
+  ];
+  var imagePath = 'url(img/bg/' + images[ Math.floor( Math.random() * images.length ) ] + ")";
+  $("#bg-container").css("background-image", imagePath);
 
-document.getElementById( "bg-container" ).style.backgroundImage = "url(" + imagePath + ')';
+  var pageVariant = Math.floor( Math.random() * 3 );
+
+  var headingVariations = [
+    "Social Media",
+    "Save Places",
+    "Aggregator"
+  ];
+  var copyVariations = [];
+
+  $(".feature-heading").text(headingVariations[pageVariant]);
+
+});
